0ea4ea20e1c69ffbe7ce4d1c3e7dfc886e8c5b4a
hiankun <hiankun@gmail.com>
2018-04-14 15:37:15 +0800

    gid -f

--- google_images_downloader/README.md
+++ google_images_downloader/README.md
@@ -1,3 +1,5 @@
 The script was using [rafpyprog/ImageSoup](https://github.com/rafpyprog/ImageSoup) as the core function to download images from Google Images.
 
 Use `python gid.py --help` to check the usage.
+
+**Note**: The images found via Google Images may be protected by copyrights. To find free images, please refer to [Find free-to-use images](https://support.google.com/websearch/answer/29508?hl=en).
--- google_images_downloader/gid.py
+++ google_images_downloader/gid.py
@@ -23,7 +23,13 @@ ap.add_argument("-k", "--search-keywords", required=True,
 NOTE: Without assign -d option, the images will be saved as
       your_keywords/your-keywords-dddd.ext.
       When -d option is given, the images will be saved as
-      dir_name/dir-name-dddd.ext.""")
+      dir_name/directory-name-dddd.ext.""")
+ap.add_argument("-f", "--file-name",
+    help="""The file name to be saved.
+NOTE: Without assign -f option, the images will be saved as
+      dir_name/your-keywords-dddd.ext.
+      When -f option is given, the images will be saved as
+      dir_name/file-name-dddd.ext.""")
 ap.add_argument("-s", "--search-size", default='any',
     help="image size to search; the default value is 'any'")
 ap.add_argument("-n", "--image-number", type=int, default='10',
@@ -34,12 +40,15 @@ args = vars(ap.parse_args())
 
 # set up
 keywords = args["search_keywords"]
+filename = args["file_name"]
 size = args["search_size"]
 number = args["image_number"]
-dir = args["saving_directory"] if args["saving_directory"] is not None else "_".join(keywords.split())
-if (os.path.exists(dir)):
-    dir += time.strftime("_%Y%m%d_%H%M%S")
-os.makedirs(dir)
+directory = ( args["saving_directory"]
+        if args["saving_directory"] is not None
+        else "_".join(keywords.split()) )
+if os.path.exists(directory):
+    directory += time.strftime("_%Y%m%d_%H%M%S")
+os.makedirs(directory)
 
 soup = ImageSoup()
 images = soup.search(keywords, image_size=size, n_images=number)
@@ -63,13 +72,18 @@ for i, img in enumerate(images):
             print("response.info() = {}".format(response.info()))
             continue
         except ValueError:
-            print("response.info()['Content-Type'] = {}".format(response.info()['Content-Type']))
+            print("response.info()['Content-Type'] = {}"
+                    .format(response.info()['Content-Type']))
             continue
         if (file_type == 'image' and file_ext != 'vnd.ms-photo'):
-            filename = "{0}/{1}_{2:04d}.{3}".format(
-                dir, "-".join(keywords.split()), i, file_ext)
-            img.to_file(filename)
-            print('{0} has been saved...'.format(filename))
+            if filename == None:
+                file_name = "{0}/{1}_{2:04d}.{3}".format(
+                        directory, "-".join(keywords.split()), i, file_ext)
+            else:
+                file_name = "{0}/{1}_{2:04d}.{3}".format(
+                        directory, "-".join(filename.split()), i, file_ext)
+            img.to_file(file_name)
+            print('{0} has been saved...'.format(file_name))
     except urllib3.exceptions.HTTPError as e:
         print('HTTPError: ', e)
         continue
