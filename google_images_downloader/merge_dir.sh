bfe8bcd825dbc361d3fc27c9dcb332aa1a52c5ac
hiankun <hiankun@gmail.com>
2018-05-17 17:05:33 +0800

    merge_dir.sh

--- google_images_downloader/merge_dir.sh
+++ google_images_downloader/merge_dir.sh
@@ -1,6 +1,7 @@
+# bash merge_dir.sh dir01/* dir02/* dirN/* merged_dir
 arg_n=${#@}
 target=${@: -1}
-for f in ${@: 1:$arg_n-1}
-    do mv -nv "$f" $target/$(md5sum -b "$f" | cut -d ' ' -f 1); 
-    #do echo "$arg_n" "$f" "$target"
+for f in ${@: 1:$arg_n-1}; do
+    ext=$(file --mime-type $f | cut -d ':' -f 2 | cut -d '/' -f 2);
+    mv -nv $f $target/$(md5sum -b "$f" | cut -d ' ' -f 1).$ext;
 done
