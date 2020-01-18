c145aa3076c89e94e1dcd497b26cf4bc13cf93a4
hiankun <hiankun@gmail.com>
2020-01-19 00:36:16 +0800

    bilateral filtering

--- cartoon_effects/cartoonify.py
+++ cartoon_effects/cartoonify.py
@@ -1,1 +1,22 @@
-pencil_sketch.py
+import numpy as np
+import cv2
+
+
+sigmaColor = 60
+sigmaSpace = 5 ## large value will slow down the program dramatically
+cap = cv2.VideoCapture(0)
+
+while True:
+  ret, frame = cap.read()
+
+  frame = cv2.resize(frame, (0,0), fx=0.5, fy=0.5)
+  frame = cv2.bilateralFilter(frame, -1, sigmaColor, sigmaSpace)
+  frame = cv2.resize(frame, (0,0), fx=2, fy=2)
+  cv2.imshow('frame', frame)
+
+  if cv2.waitKey(1) & 0xFF == ord('q'):
+    break
+
+
+cap.release()
+cv2.destroyAllWindows()
