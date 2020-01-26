afdab105e3fe28f67e9b74d7359e7ef6eb6c9858
hiankun <hiankun@gmail.com>
2020-01-26 11:59:22 +0800

    refactor

--- cartoon_effects/cartoonify.py
+++ cartoon_effects/cartoonify.py
@@ -1,28 +1,21 @@
 import numpy as np
 import cv2
+import cartoon_filters
 
 
-frame = cv2.imread('/home/thk/Desktop/trump.jpg', cv2.IMREAD_COLOR)
-ref = cv2.imread('/home/thk/Desktop/cartoon.jpg', cv2.IMREAD_COLOR)
-gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+def main():
+  frame = cv2.imread('./pics/chhaukau.jpg', cv2.IMREAD_COLOR)
+  
+  colors = cartoon_filters.get_bilateral(frame) 
+  edges = cartoon_filters.get_sketch_lines(frame)
+  
+  res = cv2.bitwise_and(colors, edges)
 
-sigmaColor = 60
-sigmaSpace = 5 ## large value will slow down the program dramatically
+  cv2.imshow('filtered_color', np.hstack((colors, edges, res)))
+  cv2.waitKey(0)
+  
+  cv2.destroyAllWindows()
 
-# Color
-#filtered_color = cv2.resize(frame, (0,0), fx=0.5, fy=0.5)
-filtered_color = cv2.bilateralFilter(frame, -1, sigmaColor, sigmaSpace)
-#filtered_color = cv2.resize(filtered_color, (0,0), fx=2, fy=2)
 
-# Sketch
-blurred = cv2.GaussianBlur(gray,(3,3),0,0)
-laplacian = cv2.Laplacian(blurred, cv2.CV_8U, ksize = 5, scale = 1, delta = 0)
-_, edges = cv2.threshold(laplacian, 120, 255, cv2.THRESH_BINARY_INV)
-edges_3ch = cv2.merge((edges,edges,edges))
-
-res = cv2.bitwise_and(filtered_color, edges_3ch)
-
-cv2.imshow('filtered_color', np.hstack((filtered_color, edges_3ch, res, ref)))
-cv2.waitKey(0)
-
-cv2.destroyAllWindows()
+if __name__=='__main__':
+  main()
