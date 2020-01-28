2cbe3bf1fa4d820dc7b8de177dd806d990223826
hiankun <hiankun@gmail.com>
2020-01-28 20:32:16 +0800

    add tolerance mode

--- chroma_keying/chroma_keying.py
+++ chroma_keying/chroma_keying.py
@@ -1,26 +1,25 @@
 import cv2
 import numpy as np
 
-maxScaleUp = 100
-scaleFactor = 1
-scaleType = 0
-maxType = 1
 
 windowName = 'Chroma Keying'
 trackbarValue = 'Scale'
 trackbarType = 'Tolerance'
+trackbarMode = 'Mode(0): -tolerance\nMode(1): +tolerance'
 
 # load an image
 in_vid = './vids/greenscreen-demo-half.mp4'
+bg_vid = './vids/chaplin_a_thief.mp4'
 bg_file = './pics/twice_bg.jpg'
 
 class Settings:
   pts = []
   patch = None
-  frame = None
+  fg = None
   bg = None
   upper = np.array([255,255,255], dtype=np.uint8)
   lower = np.array([0,0,0], dtype=np.uint8)
+  mode = 1
 
 
 def apply_mask(tol=None):
@@ -41,10 +40,10 @@ def apply_mask(tol=None):
 
   # get mask
   bg = Settings.bg
-  fg = Settings.frame
+  fg = Settings.fg
   bg_mask = cv2.inRange(fg, Settings.lower, Settings.upper).astype('uint8')
   # refine bg_mask
-  bg_mask = cv2.morphologyEx(bg_mask, cv2.MORPH_OPEN, np.ones((3,3),np.uint8),iterations=15)
+  bg_mask = cv2.morphologyEx(bg_mask, cv2.MORPH_OPEN, np.ones((3,3),np.uint8),iterations=11)
   fg_mask = cv2.bitwise_not(bg_mask)
 
   # apply mask
@@ -61,7 +60,7 @@ def colorPatchSelector(action, x, y, flags, userdata):
   if action==cv2.EVENT_LBUTTONUP:
     Settings.pts.append([x,y])
     [xmin, ymin], [xmax, ymax] = Settings.pts
-    Settings.patch = Settings.frame[ymin:ymax,xmin:xmax].copy()
+    Settings.patch = Settings.fg[ymin:ymax,xmin:xmax].copy()
     cv2.imshow('patch', Settings.patch)
     apply_mask()
     Settings.patch = None #reset patch to keep old lower/upper boundaries
@@ -72,29 +71,40 @@ def colorPatchSelector(action, x, y, flags, userdata):
 
 
 def tolerance(*args):
-  tol = args[0]
+  if Settings.mode == 1:
+    tol = args[0]
+  else:
+    tol = -args[0]
+
   apply_mask(tol)
+
+
+def tolerance_mode(*args):
+  Settings.mode = args[0]
 
 
 def main():
   cv2.namedWindow(windowName, cv2.WINDOW_NORMAL)
   cv2.setMouseCallback(windowName, colorPatchSelector)
   cv2.createTrackbar(trackbarType, windowName, 0, 255, tolerance)
+  cv2.createTrackbar(trackbarMode, windowName, 1, 1, tolerance_mode)
   
-  cap = cv2.VideoCapture(in_vid)
+  fg_cap = cv2.VideoCapture(in_vid)
+  bg_cap = cv2.VideoCapture(bg_vid)
   while True:
-    _, Settings.frame = cap.read()
-    Settings.bg = cv2.imread(bg_file, cv2.IMREAD_COLOR)
-    #Settings.frame = cv2.resize(Settings.frame, None, fx=0.25, fy=0.25)
-    #Settings.frame = cv2.cvtColor(Settings.frame, cv2.COLOR_BGR2HSV)
-    frame_h, frame_w, _ = Settings.frame.shape
-    Settings.bg = cv2.resize(Settings.bg, (frame_w, frame_h))
-    cv2.imshow(windowName, Settings.frame)
+    _, Settings.fg = fg_cap.read()
+    #Settings.bg = cv2.imread(bg_file, cv2.IMREAD_COLOR)
+    _, Settings.bg = bg_cap.read()
 
+    #Settings.fg = cv2.resize(Settings.fg, None, fx=0.25, fy=0.25)
+    #Settings.fg = cv2.cvtColor(Settings.fg, cv2.COLOR_BGR2HSV)
+    fg_h, fg_w, _ = Settings.fg.shape
+    Settings.bg = cv2.resize(Settings.bg, (fg_w, fg_h))
+    cv2.imshow(windowName, Settings.fg) 
     res = apply_mask()
     cv2.imshow('res', res)
 
-    if cv2.waitKey(33) & 0xFF == ord('q'):
+    if cv2.waitKey(0) & 0xFF == ord('q'):
       break
   
   cv2.destroyAllWindows()
