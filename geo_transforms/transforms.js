90d1716404e6a6cef34f728ce19e123769e02355
hiankun <hiankun@gmail.com>
2020-01-30 00:43:27 +0800

    transform js test

--- geo_transforms/transforms.js
+++ geo_transforms/transforms.js
@@ -13,13 +13,74 @@ var corners = [
 //drawDot({x:100, y:50}, 10, "green");
 //drawLine(corners[0], corners[1], 3, "white");
 
+
+//var a = [[-2, 3], [2, 4], [3, 6]]
+//var b = [[1, 2, 3], [4, 6, 8]]
+////print(matrixDot(a,b));
+//printMatrix(matrixDot(a,b));
+
+function printMatrix(mat) {
+  mat.forEach(m => document.write(`<br/>&nbsp;&nbsp;${m.join(' ')}`)) 
+}
+
+function matrixDot(A, B) {
+  /* source:
+  https://stackoverflow.com/a/48694670
+  */
+  var result = new Array(A.length).fill(0).map(row => new Array(B[0].length).fill(0));
+
+  return result.map((row, i) => {
+      return row.map((val, j) => {
+          return A[i].reduce((sum, elm, k) => sum + (elm*B[k][j]) ,0)
+          })
+      })
+}
+
+function scaleTransform(scale, pts) {
+  let mat = [ [scale.x, 0, 0],
+              [0, scale.y , 0] ];
+  let vec = [];
+  let transformed = [];
+  for (let i=0; i<pts.length; i++) {
+    vec = [ [pts[i].x],
+            [pts[i].y],
+            [1] ];
+    let res = matrixDot(mat, vec)
+    transformed.push({x: res[0], y: res[1]});
+  }
+
+  return transformed
+}
+function shearTransform(shear, pts) {
+  let mat = [ [1, shear, 0],
+              [0, 1 , 0] ];
+  let vec = [];
+  let transformed = [];
+  for (let i=0; i<pts.length; i++) {
+    vec = [ [pts[i].x],
+            [pts[i].y],
+            [1] ];
+    let res = matrixDot(mat, vec)
+    transformed.push({x: res[0], y: res[1]});
+  }
+
+  return transformed
+}
+
+function applyTransform(){
+  let scaleAmount = {x: document.getElementById("scaleX").value,
+                    y: document.getElementById("scaleY").value};
+  let shearAmount = document.getElementById("shearInput").value;
+  let scaled = scaleTransform(scaleAmount, corners);
+  let transformed = shearTransform(shearAmount, scaled);
+  drawPolygon(transformed, 5, "blue");
+}
+
 function clearCanvas() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  drawPolygon(corners, 3, "red");
 }
 
-
-function callDrawPolygon() {
-  drawPolygon(corners, 5, "red");
-}
 
 function drawPolygon(points, line_width, color) {
   ctx.beginPath();
