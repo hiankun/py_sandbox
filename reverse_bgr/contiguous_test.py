a0c966491d5debc24f9eb44c34255e6eb681500c
hiankun <hiankun@gmail.com>
2020-02-21 00:25:47 +0800

    test of contiguous array for bgr 2 rgb

--- reverse_bgr/contiguous_test.py
+++ reverse_bgr/contiguous_test.py
@@ -28,28 +28,28 @@ logo = cv2.resize(logo, (size,size))
 Try different methods and check the flags
 '''
 print('img: {}\n{}'.format(img.dtype, img.flags))
-img_swap = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-#img_swap = img[:,:,::-1]
-#img_swap = np.array(img[:,:,::-1])
-#img_swap = np.ascontiguousarray(img[:,:,::-1])
-print('img_swap: {}\n{}'.format(img_swap.dtype, img_swap.flags))
+#img_reverse = img[:,:,::-1]
+#img_reverse = np.array(img[:,:,::-1])
+#img_reverse = np.ascontiguousarray(img[:,:,::-1])
+img_reverse = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+print('img_reverse: {}\n{}'.format(img_reverse.dtype, img_reverse.flags))
 
 '''
 Try cv2 drawing functions
 '''
-cv2.rectangle(img_swap, top_right, bottom_left, (0,0,255), 2)
-cv2.line(img_swap, top_right, bottom_left, (0,255,0), 2)
-cv2.circle(img_swap, center, int(h/4), (255,0,0), 2)
-cv2.putText(img_swap, 'Twice', bottom_left, 3, 1.5, (0,255,255), 2)
+cv2.rectangle(img_reverse, top_right, bottom_left, (0,0,255), 2)
+cv2.line(img_reverse, top_right, bottom_left, (0,255,0), 2)
+cv2.circle(img_reverse, center, int(h/4), (255,0,0), 2)
+cv2.putText(img_reverse, 'Twice', bottom_left, 3, 1.5, (0,255,255), 2)
 
 '''
 Try overlay
 '''
-overlay = cv2.addWeighted(img_swap[h-size:h,w-size:w],0.2,logo,0.8,0)
-img_swap[h-size:h,w-size:w] = overlay
+overlay = cv2.addWeighted(img_reverse[h-size:h,w-size:w],0.2,logo,0.8,0)
+img_reverse[h-size:h,w-size:w] = overlay
 
 '''
 Show the result
 '''
-cv2.imshow('img', np.hstack((img, img_swap)))
+cv2.imshow('img', np.hstack((img, img_reverse)))
 cv2.waitKey()
