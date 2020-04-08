ffbd1c77806dbb994a5239c88011ecb04d10e79c
hiankun <hiankun@gmail.com>
2020-04-09 02:14:28 +0800

    add cv2 tools to generate pbtxt

---
+++ cv2_dnn/tools/run_tf_text_graph.sh
@@ -0,0 +1,8 @@
+WORK_PATH="/media/thk/workspace/gitlab_proj/tf_model_train/trained/20200407/tuna_ssd_incep_v2_200k_frozen/"
+python tf_text_graph_ssd.py \
+	--input ${WORK_PATH}frozen_inference_graph.pb \
+  	--output ${WORK_PATH}frozen_inference_graph.pbtxt \
+    	--config ${WORK_PATH}pipeline.config
+
+    	#--config /media/thk/workspace/gitlab_proj/tf_model_train/trained/20200407/ssd_inception_v2_coco.config
+
